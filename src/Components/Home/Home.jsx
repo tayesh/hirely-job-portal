6d893f91d936b0a61ff8825f773b7791df2ee03d
Arjon200141 <aorjon123@gmail.com>
2025-03-04 23:59:42 +0600

    Push

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "../Banner/Banner";
 import Courses from "../Courses/Courses";
+import Featured from "./Featured";
 import LiveJobs from "./LiveJobs";
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
            <Banner></Banner>
            <LiveJobs></LiveJobs>
            <Courses></Courses>
+           <Featured></Featured>
         </div>
     );
 };
