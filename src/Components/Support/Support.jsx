4fe61e066faf8dec76d9e887cec57aaf61aa8439
Arjon200141 <aorjon123@gmail.com>
2025-03-06 03:54:42 +0600

    Support Page Added

--- src/Components/Support/Support.jsx
+++ src/Components/Support/Support.jsx
@@ -1,11 +1,13 @@
 import Contact from "./Contact";
 import Cover from "./Cover";
+import Message from "./Message";
 
 const Support = () => {
     return (
         <div>
             <Cover></Cover>
             <Contact></Contact>
+            <Message></Message>
         </div>
     );
 };
