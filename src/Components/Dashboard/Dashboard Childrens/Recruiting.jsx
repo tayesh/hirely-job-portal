f8c8b43ca24e53cb8d7db5d55632487574743fa2
unknown <tayesh>
2025-03-07 04:24:55 +0600

    Dashboard UI and routes

---
+++ src/Components/Dashboard/Dashboard Childrens/Recruiting.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Recruiting = () => {
+    return (
+        <div>
+            Recruiting
+            
+        </div>
+    );
+};
+
+export default Recruiting;
